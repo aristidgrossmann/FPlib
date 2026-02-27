226c5a4709250c16a9bba11dad824b2cd5dbfd35
Aristid <aristidg@gmail.com>
2026-02-27 15:34:33 +0100

    plot with specified xlims and ylims

---
+++ examples/plots/xyLimits/plotXYLimits.py
@@ -0,0 +1,48 @@
+# region IMPORTS 
+import numpy as np
+import os
+import sys
+from matplotlib import pyplot as plt
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[3]))
+from src import fp_library
+# endregion
+
+
+
+# region DATA LOADING
+os.chdir('examples/plots/simple-no-uncertainties/data')
+
+organic_data = np.genfromtxt('Organic Szin.txt')
+channels = np.arange(1, len(organic_data)+1)
+
+
+os.chdir('..')
+
+
+
+
+##############################   PLOTTING WITH X & Y LIMITS   #############################
+
+#note that using the plotting function with uncertainties and setting xerr, yerr = np.zeros(...) is also possible
+
+#########  SPECIFICATIONS   ##########
+xdata = channels
+ydata = organic_data 
+
+xlims = (2000, 4000)
+ylims = (50, 200)
+
+title = r'$\gamma-$spectrum, organic scintillator'
+xlabel = 'Channel [-]'
+ylabel = 'Counts [-]'
+legend_loc = 'upper right'
+file_name = "plotWithXYLims"
+
+
+
+fp_library.plot_raw_data_no_uncertainty(xdata=xdata, ydata=ydata, title=title, xlabel=xlabel, ylabel=ylabel, 
+                                        legend_loc=legend_loc, xlims=xlims, ylims=ylims, file_name=file_name)
+
+
